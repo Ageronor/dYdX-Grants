b1e431af58db05826985fb832c274c322b7570d8
Roman Proshunin <proshuninreal@gmail.com>
2022-11-11 17:49:20 +0600

    fixes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.sass";
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
