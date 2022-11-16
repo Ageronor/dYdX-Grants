fac2fbce13c0c28182864fbb54b8439314cdb59e
Roman Proshunin <proshuninreal@gmail.com>
2022-11-16 09:18:46 +0600

    added FAQ block

--- src/index.js
+++ src/index.js
@@ -10,3 +10,15 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
 });
+
+let answers = document.querySelectorAll(".faq__layout");
+
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
